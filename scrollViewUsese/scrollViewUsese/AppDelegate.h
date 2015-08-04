db5c167a61bf4322b112929099f3cd0ccb4fe57f
zqq702 <zqq702@sina.com>
2015-08-04 23:15:25 +0800

    新建scrollView项目   熟悉scrollView的基本使用

---
+++ scrollViewUsese/scrollViewUsese/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollViewUsese
+//
+//  Created by 张庆庆 on 15/8/4.
+//  Copyright (c) 2015年 Zqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollViewUsese/scrollViewUsese/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  scrollViewUsese
+//
+//  Created by 张庆庆 on 15/8/4.
+//  Copyright (c) 2015年 Zqq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
